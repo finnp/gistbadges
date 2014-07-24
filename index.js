1ff55c6989d02932c7b52cea1de9c13a6ee6ff2f
Finn <derfinn@gmail.com>
2014-07-24 14:19:40 +0200

    Basic Gist Creation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var path = require('path')
 var express = require('express')
 var passport = require('passport')
 var GitHubStrategy = require('passport-github').Strategy
+var Github = require('github')
 
 var baseURL = process.env.URL || 'http://localhost:8000'
 
@@ -15,6 +16,24 @@ app.use(require('express-session')({
   }))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(function (req, res, next) {
+  req.mytest = "wow"
+  
+  console.log('req', req.user)
+  if(req.user && req.user.token) {
+    req.github = new Github(({
+      debug: true,
+      version: '3.0.0'
+    }))
+    
+    req.github.authenticate({
+      type: 'oauth',
+      token: req.user.token
+    })
+  }
+  
+  next()
+})
 
 passport.use(new GitHubStrategy({
   clientID: process.env['GITHUB_CLIENT'],
@@ -23,6 +42,7 @@ passport.use(new GitHubStrategy({
   scope: 'gist,user'
 }, function (accessToken, refreshToken, profile, done) {
   process.nextTick(function () {
+    profile.token = accessToken
     done(null, profile)
   })
 }))
@@ -48,17 +68,33 @@ app.get('/callback', passport.authenticate('github', {
 })
 
 app.get('/', function (req, res) {
-  console.log('Main', req.user)
+  // console.log('Main', req.user)
+    
+  var options = {}
   
-  var options = {
-    loggedin: req.isAuthenticated()
-  }
-  
-  if(options.loggedin) {
+  if(req.isAuthenticated()) {
+    options.loggedin = true
     options.githubName = req.user.username
+    
+    var newGist = {
+      'description': 'badge issued with GistBadges',
+      'public': true,
+      'files': {
+        'test.txt': {
+          'content': 'this is my first automatically created gist'
+        }
+      }
+    }
+    
+    req.github.gists.create(newGist, function (err, res) {
+      console.log(res)
+    })
+
+  } else {
+    options.loggedin = false
   }
   
-  res.render('issue.jade', options)
+  res.render('issue', options)
 })
 
 // endpoint for receiving the badge
